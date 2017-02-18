f887b2ce9939b1fd35848b1996c7890155d679c9
Dinesh Parimi <dinesh.parimi@live.com>
2017-02-18 10:08:53 -0800

    revert

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
+    url(r'^stressmanager/', include('stressmanager.urls')),
     url(r'^admin/', admin.site.urls),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
